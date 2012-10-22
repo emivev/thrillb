deac5bcdaf479605eb258c4056995d93e0b5218c
Emils Veveris <emils.veveris@gmail.com>
2012-10-23 00:27:59 +0300

    'removed_passets'

--- config/application.rb
+++ config/application.rb
@@ -50,6 +50,8 @@ module N1
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    
+    
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
